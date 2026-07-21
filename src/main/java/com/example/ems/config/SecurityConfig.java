9e45877ecd7775ee5305adcc46ce9f7f7f9a79ec
Shyam Butani <butanishyam111@gmail.com>
2026-07-21 20:40:07 +0530

    Add authentication using SecurityFilterChain

---
+++ src/main/java/com/example/ems/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.ems.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails admin =
+                User.withUsername("admin")
+                        .password("{noop}admin123")
+                        .roles("ADMIN")
+                        .build();
+
+        UserDetails user =
+                User.withUsername("John")
+                        .password("{noop}user123")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
